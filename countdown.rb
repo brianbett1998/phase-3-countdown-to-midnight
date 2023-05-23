71c87ca50ba8e5bbf1c4b0da6a422b96c53f5d32
BRIAN BETT <+>
2023-05-23 12:16:50 +0300

    Completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+#write your code
 
-def countdown
-end
+def countdown(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      sleep(1)
+      seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
